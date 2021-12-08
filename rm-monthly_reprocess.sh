bf7146f14b6a3bd991d19a79ced24d9961cdd139
Hiroyuki Miyazaki <heromiya@heromiya.net>
2021-12-08 08:29:44 +0000

    new file:   monthly_reprocess.lst 	new file:   rm-monthly_reprocess.sh

---
+++ rm-monthly_reprocess.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+IFS='
+'
+for ARGS in $(cat monthly_reprocess.lst); do
+    YEAR=$(echo $ARGS | cut -f 1)
+    MONTH=$(printf %02d $(echo $ARGS | cut -f 2))
+    rm monthly_mosaic/cloudfree-median.tif.d/monthly/$YEAR-$MONTH-$MONTH-cloudfree-median.tif*
+done
